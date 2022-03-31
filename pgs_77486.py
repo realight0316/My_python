5025da670233f22a6d34e44640f5aa41a9f77d5f
realight0316 <0316kjh@naver.com>
2022-03-31 23:32:00 +0900

    2022.03.31

---
+++ pgs_77486.py
@@ -0,0 +1,7 @@
+# 프로그래머스 : 다단계 칫솔 팔기 (https://programmers.co.kr/learn/courses/30/lessons/77486)
+
+# 문제 너무 길음, 링크에서 직접 설명을 위한 이미지와 함께 확인 필요
+
+def solution(enroll, referral, seller, amount):
+    answer = []
+    return answer
