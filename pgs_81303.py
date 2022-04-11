46bfbfc62bf78b6c75c141ec736fa7dbd7438875
realight0316 <0316kjh@naver.com>
2022-04-11 14:59:29 +0900

    2022.04.11

---
+++ pgs_81303.py
@@ -0,0 +1,20 @@
+# 프로그래머스 : 표 편집 (https://programmers.co.kr/learn/courses/30/lessons/81303)
+
+# 문제에 대한 설명은 링크에서 표 그림과 함꼐 이해 필요
+
+n = 8
+k = 2
+cmd = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]
+answer = "OOOOXOOO"
+
+# n = 8
+# k = 2
+# cmd = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]
+# answer = "OOXOXOOO"
+
+def solution(n, k, cmd):
+    answer = ''
+    return answer
+
+results = solution(n, k, cmd)
+print(f"{answer} / {results}")
