7f2059ecd6a9566175dfaa8c82abec0643677608
realight0316 <0316kjh@naver.com>
2022-04-13 01:34:54 +0900

    2022.04.13

--- pgs_81303.py
+++ pgs_81303.py
@@ -2,21 +2,45 @@
 
 # 문제에 대한 설명은 링크에서 표 그림과 함꼐 이해 필요
 
-n = 8
-k = 2
-cmd = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]
-answer = "OOOOXOOO"
-
 # n = 8
 # k = 2
-# cmd = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]
-# answer = "OOXOXOOO"
+# cmd = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]
+# answer = "OOOOXOOO"
+
+n = 8
+k = 2
+cmd = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]
+answer = "OOXOXOOO"
 
 def solution(n, k, cmd):
     answer = ''
+    members = {x:x for x in range(n)}
+    origin  = {x:x for x in range(n)}
+    now = k
+    del_list = []
+    print(members)
+    for order in cmd:
+        if order[0] == 'U':
+            now -= int(order[2:])
+        elif order[0] == 'D':
+            now += int(order[2:])
+        elif order[0] == 'C':
+            del members[now]
+            del_list.append(now)
+            if now > len(members):
+                now -= 1
+        else:
+            temp= int(del_list.pop())
+            members[temp] = temp
+
+    for idx in range(len(origin)):
+        try:
+            if members[idx] == origin[idx]:
+                answer += "O"
+        except:
+            answer += 'X'
+
     return answer
 
 results = solution(n, k, cmd)
 print(f"{answer} / {results}")
-
-미안하다 이거 보여주려고 뻥커밋했다
