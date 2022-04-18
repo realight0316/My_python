ace9e1549996864d09f7be9b407a2ee3fc9649c0
realight0316 <0316kjh@naver.com>
2022-04-19 02:43:39 +0900

    2022.04.19 완료 / 이분탐색, 어려움

--- pgs_86053.py
+++ pgs_86053.py
@@ -27,7 +27,31 @@ result= 50
 # b ≤ s의 모든 수의 합
 
 def solution(a, b, g, s, w, t):
-    answer = -1
+    answer = 4 * 10**14 + 10**5
+    start = 0
+    end = 4 * 10**14 + 10**5        # (2*10^5)*(2*10^9)+10^5, (편도 시간*2)*(금과 은 두번)+마지막 편도시간
+
+    while start <= end:
+        mid = (start + end) // 2
+        gmax, gmin, smax, smin = 0, 0, 0, 0     # 금 최대운반량, 금 최소운반량, 은 최대운반량, 은 최소운반량
+
+        for i, ti in enumerate(t):
+            x = (mid-ti) // (2*ti) + 1      #  해당 시간 내 가능 운송횟수 = (값-편도1회) // (왕복시간) + 앞에서 뺀 편도횟수 1
+            gmax += g[i] if g[i] - x * w[i] <= 0 else x * w[i]   # 각각 운반가능량이 소지물량을 상회하면 최대운반량은 소지물량과 같음
+            smax += s[i] if s[i] - x * w[i] <= 0 else x * w[i]   # 그렇지 않으면 운반가능량이 최대운반량이 된다
+            gmin += min(g[i], abs(s[i] - x * w[i])) if s[i] - x * w[i] <= 0 else 0   # 은(금)을 최대운반을 하고 남은 잔여 가능운반량이 금(은)의 최소운반량이다
+            smin += min(s[i], abs(g[i] - x * w[i])) if g[i] - x * w[i] <= 0 else 0   # 최대운반을 하고도 은(금)이 남았다면 더이상 금(은)은 운반할 수 없으므로 0
+
+        # 금을 a무게 이상 운반 할 수 있으면 그 아래 무게는 전부 운반 가능
+        # a+b 무게를 운반 할 수 있다면 금을 a만큼 운반하고 나머지는 b운반에 사용 가능
+        if gmax >= a and smax >= b and gmax + smin >= a + b:
+            end = mid - 1
+            try:
+                answer = min(answer, mid)
+            except:
+                answer = mid
+        else:
+            start = mid + 1
 
     return answer
 
