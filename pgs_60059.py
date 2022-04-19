ca437acd562bdffb4a978352a6f0d205f4cf8fc5
realight0316 <0316kjh@naver.com>
2022-04-20 01:53:16 +0900

    2022.04.20

---
+++ pgs_60059.py
@@ -0,0 +1,14 @@
+# 프로그래머스 : 자물쇠와 열쇠 (https://programmers.co.kr/learn/courses/30/lessons/60059)
+
+# 표 확장해서 3회 회전하며 비교하고 한칸씩 이동하기
+
+key	= [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
+lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+results = True
+
+def solution(key, lock):
+    answer = True
+    return answer
+
+answer = solution(key, lock)
+print(f"{results} / {answer}")
