f07b14fd94ac12cae644e46f998b2393e0c7a4bb
realight0316 <0316kjh@naver.com>
2022-04-21 02:32:11 +0900

    2022.04.21

--- pgs_60059.py
+++ pgs_60059.py
@@ -7,8 +7,19 @@ lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
 results = True
 
 def solution(key, lock):
-    answer = True
-    return answer
+    key_len = len(key)-1
+    lock_len = len(lock)
+    ex_len = 2*(key_len) + lock_len
+    extend = [[0 for a in range(ex_len)] for b in range(ex_len)]
+
+    for idx in range(len(extend)):
+        if key_len <= idx <= key_len + key_len:
+            for lockpiece in range(len(lock)):
+                extend[idx][key_len+lockpiece] = lock[key_len-idx][lockpiece]
+
+    for sample in extend:
+        print(sample)
+    return False
 
 answer = solution(key, lock)
 print(f"{results} / {answer}")
