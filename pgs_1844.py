ba0e3bfbf9ad459f4631808fa83c56778a880549
realight0316 <0316kjh@naver.com>
2022-04-28 22:18:59 +0900

    2022.04.28 완료 / BFS

--- pgs_1844.py
+++ pgs_1844.py
@@ -18,9 +18,42 @@ answer = 11
 # maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
 # answer = -1
 
+from collections import deque
+
 def solution(maps):
     answer = 0
-    return answer
+
+    N = len(maps)       # y 길이(세로)
+    M = len(maps[0])    # x 길이(가로)
+
+    sample = [[-1 for _ in range(M)] for _ in range(N)]     # 이동단계를 기록할 지도
+    sample[0][0] = 1                                        # 시작지점 1단 처리
+
+    # 상하좌우 이동
+    dy = [-1, 1, 0, 0]
+    dx = [ 0, 0,-1, 1]
+
+    queue = deque([[0, 0]])     # 큐에 시작지점 삽입상태로 생성
+
+    while queue:                # 큐 비워질때까지 진행
+        i, j = map(int, queue.popleft())    # 선입선출, i세로좌표 / j가로좌표
+        
+        for num in range(4):    # 상하좌우 이동처리
+            nx = j + dx[num]
+            ny = i + dy[num]
+
+            if 0 <= nx < M and 0 <= ny < N and maps[ny][nx] == 1:   # 존재하는 좌표 and 이동할 수 있는 좌표
+                if sample[ny][nx] == -1:                            # 이동기록이 없는 좌표
+                    sample[ny][nx] = sample[i][j] + 1               # 현재 좌표는 이동 전 좌표보다 + 1단계
+                    queue.append([ny, nx])                          # 이동할 수 있는 좌표라면 다음이동 시도 위해 큐 삽입
+
+    for q in maps:      # 맵 확인
+        print(q)
+    print()
+    for q in sample:    # 이동 기록 확인
+        print(q)
+
+    return sample[-1][-1]   # 가장 마지막 좌표가 도착지점, 단계 리턴
 
 results = solution(maps)
 print(f"{answer} / {results}")
