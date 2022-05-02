b41d777be25e5d99d5ac175504068b700cc157e8
realight0316 <0316kjh@naver.com>
2022-05-02 16:20:08 +0900

    2022.05.02 완료 / 스택

--- pgs_12973.py
+++ pgs_12973.py
@@ -22,10 +22,35 @@ answer = 1
 # s = 'cdcd'
 # answer = 0
 
-def solution(s):
-    answer = -1
+from collections import deque
 
-    return answer
+def solution(s):    # 리스트가 크면 연산과정이 너무 늘어남 / 폐기
+    queue = list(map(str, s))
+    idx = 1
+    while idx <= len(queue)-1:
+        if queue[idx-1] == queue[idx]:
+            del queue[idx]
+            del queue[idx-1]
+            idx = 1
+        else:
+            idx += 1
+    if len(queue) == 0:
+        return 1
+    return 0
 
-results = solution(s)
+def solution2(s):
+    s = list(map(str, s))           # 문자열 리스트화
+    queue = deque()
+    for idx in range(len(s)):       # 리스트 순회
+        if len(queue) == 0:         # 스택 비어있으면 우선 삽입
+            queue.append(s[idx])
+        elif queue[-1] == s[idx]:   # 스택 마지막요소와 현재 요소와 동일하면
+            queue.pop()             # 마지막 요소 삭제
+        else:
+            queue.append(s[idx])    # 요소 다르면 스택 삽입
+    if len(queue) == 0:             # 최종스택에서 모두 정리되었으면
+        return 1                    # 1
+    return 0                        # 아니면 0
+
+results = solution2(s)
 print(f"{answer} / {results}")
