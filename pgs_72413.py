54806baeb78b835b1da772bf641e9e307798b221
realight0316 <0316kjh@naver.com>
2022-05-03 22:07:46 +0900

    2022.05.03

---
+++ pgs_72413.py
@@ -0,0 +1,31 @@
+# 프로그래머스 : 합승 택시 요금 (https://programmers.co.kr/learn/courses/30/lessons/72413)
+
+# 이미지 확인해서 문제 이해 필요
+
+n=6
+s=4
+a=6
+b=2
+fares= [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]
+answer= 82
+
+#  n=7
+#  s=3
+#  a=4
+#  b=1
+#  fares=[[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]]
+#  answer=14
+
+#  n=6
+#  s=4
+#  a=5
+#  b=6
+#  fares=[[2,6,6], [6,3,7], [4,6,7], [6,5,11], [2,5,12], [5,3,20], [2,4,8], [4,3,9]]
+#  answer=18
+
+def solution(n, s, a, b, fares):
+    answer = 0
+    return answer
+
+results = solution(n, s, a, b, fares)
+print(f"{answer} / {results}")
