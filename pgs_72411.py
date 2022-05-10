5557c62a6228ae2c1c5f567e2b39f9dc7f80ecc8
realight0316 <0316kjh@naver.com>
2022-05-10 22:48:46 +0900

    2022.05.10

---
+++ pgs_72411.py
@@ -0,0 +1,20 @@
+# 프로그래머스 : 메뉴 리뉴얼 (https://programmers.co.kr/learn/courses/30/lessons/72411)
+
+orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+course = [2,3,4]
+answer = ["AC", "ACDE", "BCFG", "CDE"]
+
+# orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
+# course = [2,3,5]
+# answer = ["ACD", "AD", "ADE", "CD", "XYZ"]
+
+# orders = ["XYZ", "XWY", "WXA"]
+# course = [2,3,4]
+# answer = ["WX", "XY"]
+
+def solution(orders, course):
+    answer = []
+    return answer
+
+results = solution(orders, course)
+print(f"{answer} / {results}")
