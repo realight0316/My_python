505ad82816c4df30e4cadabe8f039f3be1036df1
realight0316 <0316kjh@naver.com>
2022-05-20 00:35:09 +0900

    2022.05.19 완료 / 이진법(bin)

--- pgs_12911.py
+++ pgs_12911.py
@@ -21,20 +21,16 @@ answer = 83
 
 def solution(n):
     answer = 0
-
-    bin_n = bin(n)
-    listn = list(map(int, bin_n[2:]))
-    cntn = listn.count(1)
-    x = n
-    cntx = 0
-
-    while cntn != cntx:
-        print(n, x)
-        x += 1
-        bin_x = bin(x)
-        listx = list(map(int, bin_x[2:]))
-        cntx = listx.count(1)
+    cntn = bin(n).count('1')        # n 이진화 (ex. 78 = 0b1001110)
+    x = n                           # n < x <= 1000000 으로 하나씩 올리면서 찾을 것임
+    cntx = 0                        # x 이진수의 1 갯수
+    while cntn != cntx:             # n과 x의 이진수 1갯수가 같은 순간 반복 종료
+        x += 1                      # 하나씩 올리면서 찾기
+        cntx = bin(x).count('1')    # x이진화의 1 갯수 찾기
     return x
-
+    
 results = solution(n)
 print(f"{answer} / {results}")
+
+# 더 금방 작성 할 수 있었는데 이것저것 사족을 붙여서 효율성테스트에서 몇번 떨어졌었음
+# 예상만 하지말고 한번 돌려보고 확인해보는게 필요할 듯, 알고리즘은 금방 짰음
