4a5b5daf3c204cbb009f1a951da92a6887208964
realight0316 <0316kjh@naver.com>
2022-07-07 23:43:10 +0900

    2022.07.07 완료 / 그리디 알고리즘, deque / 0707 Lucky day

--- pgs_42885.py
+++ pgs_42885.py
@@ -17,6 +17,8 @@
 # 구명보트의 무게 제한은 40kg 이상 240kg 이하입니다.
 # 구명보트의 무게 제한은 항상 사람들의 몸무게 중 최댓값보다 크게 주어지므로 사람들을 구출할 수 없는 경우는 없습니다.
 
+from collections import deque
+
 people = [70, 50, 80, 50]
 limit = 100
 answer = 3
@@ -27,10 +29,46 @@ answer = 3
 
 def solution(people, limit):
     answer = 0
+    people = deque(sorted(people))              # 효율성을 위한 deque
+    
+    while people:                               # 모든 인원 구출
+        if len(people) == 1:                    # 혼자 남아 있으면 혼자서 보트 탑승
+            answer += 1
+            return answer
+        
+        if people[0] + people[-1] <= limit:     # 잔여인원 중 가장 무거운 사람과 가장 가벼운 사람
+            people.pop()                        # 둘이서 탑승 가능하면 둘이서 태워서 먼저 보낸다
+            people.popleft()
+        else:
+            people.pop()                        # 2인이 탑승 불가능하면 현재 가장 무거운 인원을 혼자 태워 보낸다
+        answer += 1
     return answer
+
+        
+
+    
+
+    return answer
+
+# 모든 테스트 케이스는 통과하지만 효율성 테스트 불통
+def solution2(people, limit):
+    answer = 0
+    people.sort(reverse=True)
+
+    while people:
+        n = people.pop(0)
+        boat = limit - n
+
+        if boat != 0:
+            for i, m in enumerate(people):
+                if boat >= m:
+                    boat -= m
+                    people.pop(i)
+                    break
+        answer += 1
+    return answer
+
+
 
 result = solution(people, limit)
 print(f"{answer} / {result}")
-
-# Here's looking at you, kids
-# 당신의 눈동자에 건배
